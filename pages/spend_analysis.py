a723c42614118d64106e7d00f03f4a1df3b2d911
BabbarGaurav <106384585+BabbarGaurav@users.noreply.github.com>
2024-01-06 03:59:08 +0530

    Update spend_analysis.py

--- pages/spend_analysis.py
+++ pages/spend_analysis.py
@@ -1,87 +1,53 @@
-import pandas as pd
-import numpy as np
-import plotly.express as px
-import plotly.graph_objects as go
-import streamlit as st
-import streamlit_antd_components as sac
-from plotly.subplots import make_subplots
-import tarfile
+col1,_ = st.columns([1, 0.1])
 
+with col1:
+    fig_percentage_stacked = go.Figure()
 
-def filtering_function(filtered_df, occupation_f, age_group_f, income_group_f, city_f):
-    # global filtered_df
-    if not occupation_f and not age_group_f and not income_group_f and not city_f:
-         return df1.copy()
-    else:
-        if occupation_f:
-            filtered_df = filtered_df[filtered_df['occupation'].isin(occupation_f)]
-        if age_group_f:
-            filtered_df = filtered_df[filtered_df['age_group'].isin(age_group_f)]
-        if income_group_f:
-            filtered_df = filtered_df[filtered_df['income_group'].isin(income_group_f)]
-        if city_f:
-            filtered_df = filtered_df[filtered_df['city'].isin(city_f)]
-    return filtered_df
+    for payment_type in payment_type_order:
+        data_subset = avg_spend_by_category_gateway[avg_spend_by_category_gateway['payment_type'] == payment_type]
 
-tar_gz_file_path = "fact_spends.tar.gz"
-csv_file_name = "fact_spends.csv"
-with tarfile.open(tar_gz_file_path, 'r:gz') as tar:
-    tar.extract(csv_file_name)
-
-df2 = pd.read_csv(csv_file_name)
-df1 = pd.read_csv('dim_customers.csv')
-st.set_page_config(page_title='Spend Analysis', layout='wide')
-st.sidebar.markdown("""
-        <style>
-               .block-container {
-                    padding-top: 3rem;
-                }
-        </style>
-        """, unsafe_allow_html=True)
-
-
-# Calculate average spend by category and payment gateway
-avg_spend_by_category_gateway = df2.groupby(['category', 'payment_type'])['spend'].median().reset_index()
-
-# Calculate the total spend for each category to calculate percentages
-total_spend_by_category = avg_spend_by_category_gateway.groupby('category')['spend'].sum().reset_index()
-
-# Merge with the average spend DataFrame to calculate percentages
-avg_spend_by_category_gateway = pd.merge(avg_spend_by_category_gateway, total_spend_by_category,
-                                         on='category', suffixes=('_avg', '_total'))
-
-# Calculate percentage of average spend for each category and payment gateway
-avg_spend_by_category_gateway['percentage'] = (avg_spend_by_category_gateway['spend_avg'] /
-                                               avg_spend_by_category_gateway['spend_total']) * 100
-
-# Define the color order for payment types
-payment_type_order = df2['payment_type'].unique()
-
-cmap = ['#221f1f', '#b20710', '#e50914', '#f5f5f1']
-
-# Create a percentage stacked horizontal bar graph using go.Figure
-fig_percentage_stacked = go.Figure()
-
-for payment_type in payment_type_order:
-    data_subset = avg_spend_by_category_gateway[avg_spend_by_category_gateway['payment_type'] == payment_type]
-
-    fig_percentage_stacked.add_trace(
-        go.Bar(
-            x=data_subset['percentage'],
-            y=data_subset['category'],
-            orientation='h',
-            name=payment_type,
-            text=data_subset['percentage'].round(2).astype(str) + '%',  # Text for the percentage
-            textposition='inside',  # Place text inside the bars
-            marker_color=cmap[payment_type_order.tolist().index(payment_type)],
+        fig_percentage_stacked.add_trace(
+            go.Bar(
+                x=data_subset['percentage'],
+                y=data_subset['category'],
+                orientation='h',
+                name=payment_type,
+                text=data_subset['percentage'].round(2).astype(str) + '%',
+                textposition='inside',
+                marker_color=cmap[payment_type_order.tolist().index(payment_type)],
+            )
         )
+
+    # Update layout
+    fig_percentage_stacked.update_layout(
+        xaxis_title='Percentage',
+        barmode='stack',
+        margin=dict(t=20),
+        legend=dict(
+            orientation='h',
+            x=0.29,
+            y=1.15),
     )
 
-# Update layout
-fig_percentage_stacked.update_layout(
-    xaxis_title='Percentage',
-    barmode='stack',
-    margin=dict(t=40, b=20),
-    autosize=True
-)
-st.plotly_chart(fig_percentage_stacked , user_container_width=True)
+    median_avg_spend = filtered_df.groupby(['category', 'payment_type'])['spend'].median().reset_index()
+
+    fig_scatter = px.scatter(
+        median_avg_spend,
+        y='category',
+        x='payment_type',
+        size='spend',
+        color_discrete_sequence=['white'],  # Set the color to white
+        labels={'category': '', 'payment_type': '', 'spend': 'Median<br>Avg<br>Spend'},
+    )
+
+    fig_scatter.update_layout(
+        margin=dict(t=20),
+    )
+
+    tab1, tab2 = st.tabs(["Percentage Spend on Category by payment mode", " Absolute Spend on Category by payment mode"])
+
+    with tab1:
+        st.plotly_chart(fig_percentage_stacked, use_container_width=True)
+
+    with tab2:
+        st.plotly_chart(fig_scatter, use_container_width=True)
